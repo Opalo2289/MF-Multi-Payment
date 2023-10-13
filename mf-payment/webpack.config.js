31709de49449376e1e4389b2cfe312aec69609b5
Opalo1189 <enriquemaestre11@gmail.com>
2023-10-12 23:14:57 -0500

    Se logra configuracion para rutas

---
+++ mf-payment/webpack.config.js
@@ -0,0 +1,18 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+const moduleFederationConfig  = withModuleFederationPlugin({
+
+  name: 'mf-payment',
+
+  exposes: {
+    "./PaymentComponent": "./src/app/payment/payment.component.ts"
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
+
+moduleFederationConfig.output.publicPath = 'http://localhost:4202/';
+module.exports = moduleFederationConfig
